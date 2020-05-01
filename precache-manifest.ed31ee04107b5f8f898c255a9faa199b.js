0c5f8c7ba416f5118dcba4297f22d57f824d9726
Yao Jian Yap <yapyaojian731@gmail.com>
2020-05-01 20:24:23 +0800

    Updates

---
+++ precache-manifest.ed31ee04107b5f8f898c255a9faa199b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459194d3c84156a0847b71b49b9996f3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c1c5866fcd59ceb4966d",
+    "url": "/react-gh-pages/static/css/main.8ca9f7a1.chunk.css"
+  },
+  {
+    "revision": "eb8ea4a21e57048a19f2",
+    "url": "/react-gh-pages/static/js/2.ac0c961b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-gh-pages/static/js/2.ac0c961b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c5866fcd59ceb4966d",
+    "url": "/react-gh-pages/static/js/main.c39fb2e1.chunk.js"
+  },
+  {
+    "revision": "02f430f0c22065e064cd",
+    "url": "/react-gh-pages/static/js/runtime-main.863b1ebe.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-gh-pages/static/media/image.d7265326.png"
+  }
+]);
