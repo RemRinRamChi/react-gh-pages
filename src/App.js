ddb914ed93be62961bea07f860fc2be1743e93e1
Yao Jian Yap <yapyaojian731@gmail.com>
2020-05-01 17:37:23 +0800

    Simple api call with destructuring

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api"; // immediately goes to search for index file
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
