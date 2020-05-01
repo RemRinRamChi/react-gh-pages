a9dd228547fecf4d37738dfb6b591c8c37605d9e
Yao Jian Yap <yapyaojian731@gmail.com>
2020-05-01 17:10:43 +0800

    Implemented basic app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"; // only needed in this file
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root")); // render in the public/index.html file
